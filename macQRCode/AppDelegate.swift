606e27ff02f5beafa998b76170512c24f186edc4
Francesco Piraneo Giuliano <fpiraneo@zriemann.ch>
2020-07-20 16:56:04 +0200

    Initial Commit

---
+++ macQRCode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macQRCode
+//
+//  Created by Francesco Piraneo G. on 20.07.20.
+//  Copyright © 2020 Francesco Piraneo G. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macQRCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macQRCode
+//
+//  Created by Francesco Piraneo G. on 20.07.20.
+//  Copyright © 2020 Francesco Piraneo G. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
